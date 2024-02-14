e6d7187757e4efca58f45b76cae6306fc707b0eb
Zaid Hamid <zaidhamid600@gmail.com>
2024-02-14 10:28:44 +0300

    adding register

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors"
 import "dotenv/config"
 
 import mongoose from "mongoose"
-
+import userRoute from "./routes/user";
 mongoose.connect(process.env.MONGO as string)
 
 
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use("/api/users", userRoute);
 
 app.listen(5000, () => {
   console.log("server connected to MangoDb");
